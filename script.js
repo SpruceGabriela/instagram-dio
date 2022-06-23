4a6bad5fc06bc8a682455ac437774278ed6213d6
alexandrealecode <alecodeit@gmail.com>
2022-06-23 02:17:38 -0300

    Recriando_minha_tela_Instagram

---
+++ script.js
@@ -0,0 +1,28 @@
+let time = 5000,
+    currentImageIndex = 0,
+    images = document
+                .querySelectorAll("#slider img")
+    max = images.length;
+
+function nextImage() {
+
+    images[currentImageIndex]
+        .classList.remove("selected")
+
+    currentImageIndex++
+
+    if(currentImageIndex >= max)
+        currentImageIndex = 0
+
+    images[currentImageIndex]
+        .classList.add("selected")
+}
+
+function start() {
+    setInterval(() => {
+        // troca de image
+        nextImage()
+    }, time)
+}
+
+window.addEventListener("load", start)
